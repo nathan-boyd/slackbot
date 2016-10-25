22385c61103a64f325401a4a03be4e838726278f
Nathan Boyd <nboyd@salesforce.com>
2016-10-25 11:01:16 -0400

    updates

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-var Slack = require('slack-node')
-var apiToken = 'xoxb-95783990691-0eFye0weAOaYzz9SGYiNTUEq'
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS
 
-var slack = new Slack(apiToken)
+var RtmClient = require('@slack/client').RtmClient
+var MemoryDataStore = require('@slack/client').MemoryDataStore
 
-slack.api('users.list', function (err, response) {
-  if (err) {
-    throw err
+var token = process.env.SLACK_API_TOKEN || 'xoxb-95783990691-0eFye0weAOaYzz9SGYiNTUEq'
+
+var rtm = new RtmClient(
+  token,
+  {
+    logLevel: 'debug'
   }
-  console.log(response)
+)
+
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`)
 })
 
-slack.api('chat.postMessage', {
-  text: 'hello from nodejs',
-  channel: '@nboyd'
-}, function (err, response) {
-  if (err) {
-    throw err
-  }
-  console.log(response)
+rtm.on(RTM_EVENTS.MESSAGE, function (message) {
+  console.dir(message)
 })
+
+rtm.start()
